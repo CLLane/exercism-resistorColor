84c548a94fdef30d725e4d6d4243110adb0fb3d5
Chris Lane <pankokrumbs@gmail.com>
2020-01-13 13:26:59 -0700

    Solution

--- resistor-color.js
+++ resistor-color.js
@@ -2,9 +2,13 @@
 // This is only a SKELETON file for the 'Resistor Color' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
-
-export const colorCode = () => {
-  throw new Error("Remove this statement and implement this function");
+export const colorCode = (color) => {
+   return COLORS.reduce((acc,currentColor, i) => {
+      if(currentColor === color) {
+        acc = i
+      }
+      return acc
+    }, null)
 };
 
-export const COLORS = undefined;
+export const COLORS = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"];
